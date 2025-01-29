803ff44dc557b8e961734d529ea89218d30ebc08
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-01-29 11:09:47 +0100

    change configuration structure, component.py added sync actions, saving data to table, requirements.txt

--- src/configuration.py
+++ src/configuration.py
@@ -1,23 +1,39 @@
-from pydantic import BaseModel, Field, ValidationError
+from enum import Enum
+from pydantic import BaseModel, Field, ValidationError, computed_field
 from keboola.component.exceptions import UserException
 
 
+class LoadType(str, Enum):
+    full_load = "full_load"
+    incremental_load = "incremental_load"
+
+
 class Credentials(BaseModel):
-    x_tenant_id: str = Field(alias="xTenantId")
-    client_id: str = Field(alias="clientId")
-    client_secret: str = Field(alias="#clientSecret")
+    tenant_id: str = Field()
+    client_id: str = Field()
+    client_secret: str = Field(alias="#client_secret")
 
 
-class RetrieveSettings(BaseModel):
+class Source(BaseModel):
     module: str
-    sub_module: str = Field(alias="subModule")
-    filter_xml: str = Field(alias="filterXML")
+    sub_module: str = Field()
+    filter_xml: str = Field()
+
+
+class Destination(BaseModel):
+    table_name: str = Field(default=None)
+    load_type: LoadType = Field(default=LoadType.incremental_load)
+
+    @computed_field
+    def incremental(self) -> bool:
+        return self.load_type == LoadType.incremental_load
 
 
 class Configuration(BaseModel):
-    debug: bool = Field(title="Debug mode", default=False)
     credentials: Credentials
-    retrieve_settings: RetrieveSettings = Field(alias="retrieveSettings")
+    source: Source
+    destination: Destination
+    debug: bool = Field(title="Debug mode", default=False)
 
     def __init__(self, **data):
         try:
